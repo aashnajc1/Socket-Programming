dfb3eef57f92d53f4a8a6e3bbe400f5d071692a5
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-05-17 17:31:11 +0530

    ,

--- UDP Client.py
+++
@@ -1,7 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = socket.gethostname()
-port = 6789
-s.connect((host,port))
-message = b'Hello'
-s.sendto(message,(host,port))
