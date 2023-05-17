06d81a0c9c00f8201b3f3a092b859a438b86b7bc
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-05-17 17:31:18 +0530

    ,

--- UDP Server.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = socket.gethostname()
-port = 6789
-s.bind((host,port))
-while True:
-    c,addr = s.recvfrom(1024)
-    print("Message : ", c)
